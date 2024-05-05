670fe6db67280922b68bb16ecbf458406d421fd1
Kiki Dana <kikidana13@gmail.com>
2024-05-05 20:25:02 +0700

    Belajar Golang dasar Day 1

---
+++ Belajar-Golang-Dasar/go.mod
@@ -0,0 +1,3 @@
+module Belajar-Golang-Dasar
+
+go 1.21.6
---
+++ Belajar-Golang-Dasar/number.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Nomor satu = ", 1)
+	fmt.Println("Nomor dua = ", 2)
+	fmt.Println("Nomor tiga koma lima = ", 3.5)
+}
